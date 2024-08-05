71806152335127057fbd729a5198b6e740e4486c
Haruna2004 <harunafaruk2004@gmail.com>
2024-08-05 12:45:57 +0100

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "*",
         headers: [
           {
             key: "Access-Control-Allow-Origin",
